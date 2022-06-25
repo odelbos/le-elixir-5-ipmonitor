6b16ee59363a26dc5aee1e1a6af8d0028516a019
odelbos <od@phibox.com>
2022-06-25 11:07:45 +0200

    add basic mix info

--- mix.exs
+++ mix.exs
@@ -1,17 +1,23 @@
 defmodule IpMonitor.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @scm_url "https://github.com/odelbos/le-elixir-5-ipmonitor"
+
   def project do
     [
       app: :ip_monitor,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "Ip Monitor",
+      source_url: @scm_url,
+      description: "Monitoring IP change, using external service."
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +25,18 @@ defmodule IpMonitor.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  # -----
+
+  defp package do
+    [
+      maintainers: ["Olivier Delbos"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @scm_url}
     ]
   end
 end
