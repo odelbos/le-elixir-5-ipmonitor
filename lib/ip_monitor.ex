5f853e098a9f7b5d22605ed43a26cc1e59c445b3
odelbos <od@phibox.com>
2022-06-25 10:50:18 +0200

    initial commit

---
+++ lib/ip_monitor.ex
@@ -0,0 +1,18 @@
+defmodule IpMonitor do
+  @moduledoc """
+  Documentation for `IpMonitor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IpMonitor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ip_monitor_test.exs
@@ -0,0 +1,8 @@
+defmodule IpMonitorTest do
+  use ExUnit.Case
+  doctest IpMonitor
+
+  test "greets the world" do
+    assert IpMonitor.hello() == :world
+  end
+end
